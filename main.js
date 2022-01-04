7e3ed6be9d4adfc25eb1bc308404b286336f984c
CoderADV <97115399+CoderADV@users.noreply.github.com>
2022-01-04 21:56:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function function1() {
+    document.getElementById("cradles").play();
+}
+		function function2() {
+   document.getElementById("iphone_ringtone").play();
+}
+		function function3() {
+   document.getElementById("pikachu").play();
+}
+		function function4() {
+   document.getElementById("shape_of_you").play();
+}
+		function function5() {
+   document.getElementById("Taki_Taki").play();
+}
